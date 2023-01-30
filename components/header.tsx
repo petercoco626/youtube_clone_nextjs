c79a5a4e2fb1fbbbf2bc8012184fa8fa1bb8c16d
petercoco626 <petercoco626@gmail.com>
2023-01-31 00:48:42 +0900

    [feat] set-up url directory

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <div>
+      <Image></Image>
+      <form>
+        <input></input>
+        <div></div>
+      </form>
+    </div>
+  );
+}
---
+++ pages/videos/watch/[id].tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Watch: NextPage = () => {
+  return <div>watch</div>;
+};
+
+export default Watch;
